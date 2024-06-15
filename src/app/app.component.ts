48c3ca642d3cf403e188202edae87b58932bfc8f
gesielbr <gesiel.br@gmail.com>
2024-06-15 13:45:23 -0300

    output - parent to child communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from '../child/child.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, ChildComponent],
 })
 export class AppComponent {
   title = 'inputOutputAng18';
+
+  itemsArr: string[] = ['Item 1', 'Item 2', 'Item 3'];
+
+  addItem() {
+    const newItem = `Item ${this.itemsArr.length + 1}`;
+    this.itemsArr.push(newItem);
+
+    console.log(newItem);
+  }
 }
